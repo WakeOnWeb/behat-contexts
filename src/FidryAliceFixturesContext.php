dd66e48ff17c694033cec41c08851fa65c84540d
py stephane <py.stephane1@gmail.com>
2019-02-21 18:22:14 +0100

    Can load many fixture files.

--- src/FidryAliceFixturesContext.php
+++ src/FidryAliceFixturesContext.php
@@ -3,6 +3,7 @@
 namespace WakeOnWeb\BehatContexts;
 
 use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
 use Behat\Symfony2Extension\Context\KernelDictionary;
 
 /**
@@ -30,15 +31,37 @@ class FidryAliceFixturesContext implements Context
     }
 
     /**
+     * @Given I use fixture files:
+     */
+    public function iUseFixtureFiles(TableNode $table)
+    {
+        $files = [];
+
+        foreach ($table->getRows() as $file) {
+            $files[] = $this->getFilepath(current($file));
+        }
+
+        $this->loadFiles($files);
+    }
+
+    /**
      * @Given I use fixture file :filename
      */
     public function iUserFixtureFile(string $filename)
     {
-        $filename = $this->getFilepath($filename);
+        $this->loadFiles([$this->getFilepath($filename)]);
+    }
 
+    /**
+     * @param array $files files
+     *
+     * @return void
+     */
+    private function loadFiles(array $files): void
+    {
         $em     = $this->getDoctrine();
         $loader = $this->getContainer()->get('fidry_alice_data_fixtures.loader.doctrine');
-        $loader->load([$filename]);
+        $loader->load($files);
 
         $em->flush();
         $em->clear();
